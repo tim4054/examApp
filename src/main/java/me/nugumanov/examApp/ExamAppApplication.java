c301426560238f3f6fc7829897292c8b2c17b751
tim40 <tim4054@gmail.com>
2024-08-22 19:42:49 +0300

    Initial commit

---
+++ src/main/java/me/nugumanov/examApp/ExamAppApplication.java
@@ -0,0 +1,13 @@
+package me.nugumanov.examApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/nugumanov/examApp/ExamAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nugumanov.examApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
